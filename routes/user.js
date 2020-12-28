bd19c328cca28604eff04dce2f3043601a3b8a2b
eazyt <ramohlalen@gmail.com>
2020-12-28 19:49:24 +0200

    ejs partials and password hashing bcrypt

---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const User = require('../models/user')
+const mongoose = require('mongoose')
+
+router.get('/register', (req, res) => { 
+  res.render('accounts/register')
+})
+
+
+router.post('/register', (req, res, next) => {
+  const user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+
+  User.findOne({ email: req.body.email }, (err, userExists) => {
+    if (userExists) {
+      req.flash('errors', 'Email already exists')
+      return res.redirect('/register')
+
+    } else {
+      user.save((err) => {
+        if (err) return next(err);
+       return req.redirect('/')
+      })
+    }
+  })
+})
+  
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,62 @@
 require('dotenv').config()
 const express = require("express")
-const app = express()
+const morgan = require('morgan')
+const mogoose = require('mongoose')
+const bodyParser = require('body-parser')
+const User = require('./models/user')
+const ejs = require('ejs')
+const engine = require('ejs-mate')
 
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const flash = require('express-flash')
+
+const MONGODB_URL = process.env.DB_URI
 const PORT = process.env.PORT 
 
-app.get('/', (req, res) => { 
-  res.send('Hello World')
+const app = express()
+
+mogoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
 })
+  .then(() => { 
+    console.log(`succefully connected to the Database`)
+  })
+  .catch((e) => { 
+    console.log(`could not connect to database`, e)
+  })
+
+// Morgan Middleware
+app.use(express.static(__dirname + '/public'))
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'secret'
+}))
+app.use(flash())
+
+
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); // so you can render('index')
+
+const mainRoutes = require('./routes/main')
+const userRoutes = require('./routes/user')
+
+app.use(mainRoutes);
+app.use(userRoutes);
+
 
 app.listen(PORT, (err) => { 
   if (err) console.log('THERE WAS AN ERROR');
   console.log(`Listening on port ${PORT}`);
-  
 })
