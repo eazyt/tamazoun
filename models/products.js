b36d2ce1064f4093b58933b04e13398fe53091a5
eazyt <ramohlalen@gmail.com>
2020-12-30 12:30:31 +0200

    added models for product and category

---
+++ models/products.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  category: {
+    // type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  },
+  name: String,
+  price: Number,
+  image: String
+  
+});
+
+module.exports = mongoose.model('Product', ProductSchema)
