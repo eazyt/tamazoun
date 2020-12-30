dbb015c7a46915bd762c337f3d74bba2df8a7959
eazyt <ramohlalen@gmail.com>
2020-12-30 18:07:05 +0200

    instant search and display on search page

--- public/js/custom.js
+++ public/js/custom.js
@@ -14,7 +14,21 @@ $(function () {
         const data = json.hits.hits.map((hit) => {
           return hit;
         })
-        console.log(data)
+        $('#searchResults').empty();
+        for (let i = 0; i < data.length; i++) { 
+          let html = "";
+          html += '<div class="col-md-4">';
+          html += '<a href="/product/' + data[i]._source._id + '">';
+          html += '<div class="thumbnail">';
+          html += '<img src="' + data[i]._source.image + '">';
+          html += '<div class="caption">';
+          html += '<h3>' + data[i]._source.name + '</h3>';
+          html += '<p>' + data[i]._source.category.name + '</p>';
+          html += '<p>' + data[i]._source.price + '</p>';
+          html += '</div></div></a></div>';
+
+          $('#searchResults').append(html);
+        }
       },
       error: function (error) { 
         console.log(error)
