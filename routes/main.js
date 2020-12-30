a366174193edec650d0a2ab9f73097a995bc2b4e
eazyt <ramohlalen@gmail.com>
2020-12-30 13:17:51 +0200

    added category display view and product findbyid route

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Product = require('../models/products')
 
 router.get('/', (req, res) => {
   res.render('main/home')
@@ -7,6 +8,23 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
   res.render('main/about')
 })
+
+router.get('/products/:id', async function(req, res, next) {
+  // console.log(req.params.id)
+  Product.find({ category: req.params.id })
+    .populate('category')
+    .exec((err, products) => { 
+      if (err) return next(err + 'ERROR FROM THEN PRODUCT FIND BY ID')
+        res.render('main/category', {
+          products: products
+        })
+    })
+    .catch((err) => { 
+      console.log(err + 'ERROR FROM CATCH PRODUCT FIND BY ID')
+    })
+
+
+});
 
 
 module.exports = router;
