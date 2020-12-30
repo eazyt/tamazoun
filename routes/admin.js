b344d4cf02b526d95681c92112490c63a32969a5
eazyt <ramohlalen@gmail.com>
2020-12-30 12:34:35 +0200

    added admin routes for adding category

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const Category = require('../models/category')
+
+router.get('/add-category', function (req, res) { 
+  res.render('admin/add-category', { message: req.flash('success') });
+})
+
+router.post('/add-category', function (req, res, next) {
+  let category = new Category();
+  category.name = req.body.name;
+
+  category.save(function (err) { 
+    if (err) return next(err);
+    req.flash('success', 'Successfully added a category');
+    return res.redirect('/add-category')
+  })
+})
+
+module.exports = router
