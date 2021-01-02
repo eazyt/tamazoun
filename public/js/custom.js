587614c7aae7a58efe4e0ada6635498b4d5c3e28
eazyt <ramohlalen@gmail.com>
2021-01-02 16:30:42 +0200

    added plus and minus logic for the product view page

--- public/js/custom.js
+++ public/js/custom.js
@@ -34,8 +34,39 @@ $(function () {
         console.log(error)
       }
     })
+  })
+
+  $(document).on('click', '#plus', function (e) { 
+    e.preventDefault();
+    let priceValue = parseFloat($('#priceValue').val());
+    let quantity = parseInt($('#quantity').val());
+
+    priceValue += parseFloat($('#priceHidden').val());
+    quantity += 1;
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  })
+    
+
+  $(document).on('click', '#minus', function (e) { 
+    e.preventDefault();
+    let priceValue = parseFloat($('#priceValue').val());
+    let quantity = parseInt($('#quantity').val());
+
+    if (quantity == 1) {
+      priceValue = $('#priceHidden').val();
+      quantity = 1;
+    } else { 
+      priceValue -= parseFloat($('#priceHidden').val());
+      quantity -= 1;
+    }
 
 
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
   })
     
     
